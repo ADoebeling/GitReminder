905eb2d8d2e40fcb91cb2ee05314798676b0bcc2
lb1601com <lb@1601.com>
2016-08-15 17:36:41 +0200

    Now the script only runs, if is over 6 o'clock in the morning and before 12 o'clock at night.

--- script/gitreminder.php
+++ script/gitreminder.php
@@ -1,14 +1,17 @@
 <?php
 require_once('../bootstrap.php');
 
-    $gitReminder = new gitReminder();
-    $gitReminder
-        ->setGitHubAccount(GITREMINDER_NAME, GITREMINDER_PASSWD)
-        ->loadGhNotifications(GITREMINDER_NAME)
-        ->parseSourceText(GITREMINDER_NAME)
-        ->checkActionLimitPerRun()
-		->checkActionLimit()
-        ->markNotificationAsRead()
-        ->process()
-        ->displayTasks();
+    if(date('G',time()) > 6){
+        $gitReminder = new gitReminder();
+        $gitReminder
+            ->setGitHubAccount(GITREMINDER_NAME, GITREMINDER_PASSWD)
+            ->loadGhNotifications(GITREMINDER_NAME)
+            ->parseSourceText(GITREMINDER_NAME)
+            ->checkActionLimitPerRun()
+            ->checkActionLimit()
+            ->markNotificationAsRead()
+            ->process()
+            ->displayTasks();
+    }
+
 
